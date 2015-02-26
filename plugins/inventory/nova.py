9b29fd7e8a5d685d5654d1486fe008313cff0bc0
Monty Taylor <mordred@inaugust.com>
2015-02-26 11:59:00 -0500

    Add documentation for the new OpenStack Inventory

--- plugins/inventory/nova.py
+++ plugins/inventory/nova.py
@@ -17,6 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
+# This inventory module is DEPRECATED. Please consider moving to using the
+# openstack.py inventory module.
+
 import sys
 import re
 import os
